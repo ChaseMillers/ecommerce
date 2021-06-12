114b961c1a0373bdd02970d3a68457f322450571
Chase Miller <chasemillers@outlook.com>
2021-06-11 19:35:30 -0700

    added test

--- test/app.test.js
+++ test/app.test.js
@@ -1,5 +1,3 @@
-const divide = require('../index');
-const { expect } = require('chai');
 const supertest = require('supertest');
 const app = require('../app');
 
@@ -10,31 +8,3 @@ describe('Express App', () => {
       .expect(200, 'Hello Express!');
   });
 });
-
-describe('Divide function', () => {
-    it('should divide positive integers correctly', () => {
-        // define inputs
-        const a = 8;
-        const b = 4;
-        const expectedAnswer = 2;
-      
-        // invoke the function
-        const actualAnswer = divide(a, b);
-      
-        // assert that expected === actual
-        expect(actualAnswer).to.equal(expectedAnswer);
-      });
-
-    it('should throw an error when divide by zero', () => {
-        // define inputs
-        const a = 8, b = 0;
-      
-        // set up the function call
-        const fn = () => {
-          divide(a, b)
-        };
-      
-        // assert that exception is thrown
-        expect(fn).to.throw();
-      });
-  });
